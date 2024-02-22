5753019d5802b5b38d50cf86eb2390b434fda5bf
Vinay-web976 <vinaydgowda3@gmail.com>
2024-02-22 19:37:50 +0530

    episode 2 code along

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 let root = ReactDOM.createRoot(document.getElementById("root"));
 let parent = React.createElement("h1", {}, "Hello World");
 
